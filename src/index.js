c7b2c5d4d0c902458368f599b8e0f389e3ef6aff
Aleksandar Ivanov <425981@student.fontys.nl>
2021-08-21 00:43:40 +0300

    Removed store from index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById("root")
 );
